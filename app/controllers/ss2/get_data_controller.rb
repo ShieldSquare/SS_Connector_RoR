163716caf3ebf63c78407ebedc37034b7d11bcb4
Narasimha12 <narasimha.m@shieldsquare.com>
2018-05-17 11:47:13 +0530

    Added a fix for authenticate_user issue

--- app/controllers/ss2/get_data_controller.rb
+++ app/controllers/ss2/get_data_controller.rb
@@ -1,16 +1,16 @@
 module Ss2
 	class GetDataController < ApplicationController
-		skip_before_filter :authenticate_user!
+		skip_before_action :authenticate_user!, raise: false
 
 		def create
 			if params['jsonString'] != ""
 				error_code = Ss2.send_js_request request, params
-			end	
+			end
 	    respond_to do |format|
 	      format.json {
 	        render :json => { error_code: error_code}
 	      }
-	    end			
+	    end
 		end
 	end
 end
